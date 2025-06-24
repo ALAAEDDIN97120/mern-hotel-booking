3f817f6dc8e4e2f75cccf9573daadd7ecd494db9
‪Alaaddin yahia <150624261+ALAAEDDIN97120@users.noreply.github.com>
2025-06-24 20:23:19 +0300

    feat: add error handler

--- api/index.js
+++ api/index.js
@@ -1,13 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+//ROUTERS
 import authRoute from "./routes/auth.js";
 import usersRoute from "./routes/users.js";
 import hotelsRoute from "./routes/hotels.js";
 import roomsRoute from "./routes/rooms.js";
 
 const app = express();
-app.use(express.json());
 dotenv.config();
 
 const connect = async () => {
@@ -24,12 +24,24 @@ mongoose.connection.on("disconnected", () => {
 });
 
 //middlewares
+app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/hotels", hotelsRoute);
 app.use("/api/rooms", roomsRoute);
 
-app.listen(8000, () => {
+app.use((err, req, res, next) => {
+	const errorStatus = err.status || 500;
+	const errorMessage = err.message || "something went wrong!";
+	return res.status(errorStatus).json({
+		success: false,
+		status: errorStatus,
+		message: errorMessage,
+		stack: err.stack,
+	});
+});
+
+app.listen(8800, () => {
 	connect();
-	console.log("connected to BackEnd");
+	console.log("Server is running on port 8800");
 });
