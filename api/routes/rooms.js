7edee4249d69dad5a4edd92469be3f8e46e21fb4
‪Alaaddin yahia <150624261+ALAAEDDIN97120@users.noreply.github.com>
2025-06-27 11:18:26 +0300

    feat: complete backend architecture

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,4 +1,28 @@
 import express from "express";
+import {
+	createRoom,
+	updateRoom,
+	deleteRoom,
+	getRoom,
+	getAllRooms,
+} from "../controllers/room.controller.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
+
 const router = express.Router();
+
+//CREATE
+router.post("/:hotelid", verifyAdmin, createRoom);
+
+//UPDATE
+router.put("/:id", verifyAdmin, updateRoom);
+
+//DELETE
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
+
+//GET
+router.get("/:id", getRoom);
+
+//GETALL
+router.get("/", getAllRooms);
 
 export default router;
